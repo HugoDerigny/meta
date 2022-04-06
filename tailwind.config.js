171c479bf0d4d6830721441408d7b7d3dd4bc38d
HugoDerigny <hugo.derigny@free.fr>
2022-04-06 22:01:07 +0200

    all

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+	content: ['./src/**/*.{html,js,jsx,ts,tsx}', './pages/**/*.{html,js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				gray: colors.stone,
+			},
+			fontFamily: {
+				sans: ['"Josefin Sans"'],
+			},
+		},
+	},
+	plugins: [],
+}
