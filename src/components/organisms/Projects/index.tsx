53bdf715b8bf7774e38f3543b22909f4183d1691
HugoDerigny <hugo.derigny@free.fr>
2022-10-10 11:16:02 +0200

    feat: responsive

--- src/components/organisms/Projects/index.tsx
+++ src/components/organisms/Projects/index.tsx
@@ -10,9 +10,9 @@ export const Projects: FC<ProjectsProps> = ({ projects }) => {
 	const [showMore, setShowMore] = useState<boolean>(false)
 
 	return (
-		<div className='flex flex-wrap -mx-4'>
+		<div className='flex lg:flex-wrap overflow-auto -mx-8 px-8 snap-x'>
 			{(showMore ? projects : projects.slice(0, 6)).map((project, key) => (
-				<div key={key} className='w-1/3 p-4 shrink-0'>
+				<div key={key} className='w-full lg:w-1/3 shrink-0 p-4 snap-center'>
 					<ProjectCard project={project} />
 				</div>
 			))}
